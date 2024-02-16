e5dca2dac48f62d79a279fe2037fddbc849be9e5
labuser <connectwithpuli@gmail.com>
2024-02-16 06:51:16 +0000

    sec commit

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 // app.js 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function () {
-    navigator.serviceWorker.register("/serviceWorker.js", { scope: "/" })
+    // navigator.serviceWorker.register("/serviceWorker.js", { scope: "/" })
+    navigator.serviceWorker.register("serviceWorker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   });
